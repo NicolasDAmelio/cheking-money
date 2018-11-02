c3392d403e0262b40af6ed8542eefd750343ff60
Nicolas D'Amelio <ndamelio@capitaria.com>
2018-11-01 21:08:03 -0300

    First Commit

---
+++ src/main/java/com/ndamelio/checkingmoney/CheckingMoneyApplication.java
@@ -0,0 +1,12 @@
+package com.ndamelio.checkingmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckingMoneyApplication.class, args);
+    }
+}
